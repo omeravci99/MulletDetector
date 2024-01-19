f9bc632013b4447b8b8677592e36017747a22c25
Toprak Efe Akkılıç <efe.akkilic@ozu.edu.tr>
2024-01-20 01:48:12 +0300

    Iterated on previous code.

--- src/hardware.py
+++ src/hardware.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 #import rospy
+import torch
 import pyzed.sl as sl
 from typing import Any
 import numpy as np
-import torch
 import cv2
 import time
 """from sensor_msgs.msg import Image
@@ -29,10 +29,6 @@ def init_zed():
         print("Zed Camera Started!")
         return zed
 
-def load_model(model_name):
-    model = torch.hub.load("ultralytics/yolov5","custom", path=model_name, force_reload=True)
-    return model
-
 def detector(model,frame,device):
     model.to(device)
     frame = [frame]
@@ -41,65 +37,77 @@ def detector(model,frame,device):
     return labels, cord
 
 def main():
-    model_name = "model/yolov5/yolov5l.pt"
-    device = 'cuda' if torch.cuda.is_available() else "! cpu" 
 
-    # İnitalizing zed and setting parameters
+
+    print("Initializing Camera...")
     zed = init_zed()
-    runtime_params = sl.RuntimeParameters()
-    image_left = sl.Mat()
-    depth_left = sl.Mat()
-    point_cloud = sl.Mat()
 
-    # Setting object detection parameters
+    cap = cv2.VideoCapture(0)
+    if cap.isOpened() == 0:
+        exit(-1)
+    
+    model = torch.hub.load(("ultralytics/yolov5"), "yolov5m")
+    runtime_params = sl.RuntimeParameters()
+    image_left, depth_left, point_cloud = sl.Mat(), sl.Mat(), sl.Mat()
     obj_params = sl.ObjectDetectionParameters()
     obj_params.detection_model = sl.OBJECT_DETECTION_MODEL.CUSTOM_BOX_OBJECTS
-    zed.enable_object_detection(obj_params)
-
-    
     obj_runtime_params = sl.ObjectDetectionRuntimeParameters()
 
-    model = load_model(model_name)
-
-
-
-
-
+    print("Enabling object detection module.")
+    zed.enable_object_detection(obj_params)
+    objects = sl.Objects()
+    device = 'cuda' if torch.cuda.is_available() else "! cpu"
     while True:
         if zed.grab(runtime_params) == sl.ERROR_CODE.SUCCESS:
-
             zed.retrieve_image(image_left, sl.VIEW.LEFT)
             zed.retrieve_measure(depth_left, sl.MEASURE.DEPTH)
             zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
+            labels, cords = detector(model, frame, device)
 
-            image = image_left.get_data()
-            labels, cords = detector(model, image, device)
+            retval, frame = cap.read()
+            left_right_image = np.split(frame, 2, axis=1)
 
-
+            label_list = labels.tolist()
+            cord_list = cords.tolist()
             objects_in = []
             # The "detections" variable contains your custom 2D detections
-            for label,cord in zip(labels,cords):
-                tmp = sl.CustomBoxObjectData()
-                # Fill the detections into the correct SDK format
+            for label,cord in zip(label_list,cord_list):
+                # Create object data and fill the detections into the correct SDK format
+                """tmp = sl.CustomBoxObjectData()
                 tmp.unique_object_id = sl.generate_unique_id()
-                tmp.probability = cord[4]
-                tmp.label = int(label)
-                tmp.bounding_box_2d = cord[:4]
-                tmp.is_grounded = True # objects are moving on the floor plane and tracked in 2D only
-                objects_in.append(tmp)
-            zed.ingest_custom_box_objects(objects_in)
+                tmp.label = int(label)"""
+                
+                # Assuming cord is in the format [xmin, ymin, xmax, ymax]
+                xmin, ymin, xmax, ymax, conf = cord
+                #tmp.probability = conf
+                # Convert to [top-left, top-right, bottom-right, bottom-left]
+                top_left = [xmin, ymin]
+                top_right = [xmax, ymin]
+                bottom_right = [xmax, ymax]
+                bottom_left = [xmin, ymax]
+
+                # Arrange as [top-left, top-right, bottom-right, bottom-left]
+                #tmp.bounding_box_2d = np.array([top_left, top_right, bottom_right, bottom_left])
+                top_left = (int(xmin), int(ymin))
+                bottom_right = (int(xmax), int(ymax))
+                cv2.rectangle(left_right_image[0], top_left, bottom_right, (0,255,0), 2)
+                #tmp.is_grounded = True # objects are moving on the floor plane and tracked in 2D only
+                #objects_in.append(tmp)
 
             
-            objects = sl.Objects() # Structure containing all the detected objects
+            cv2.imshow("frame", left_right_image[0])
+            
+            print(objects_in)
+            #zed.ingest_custom_box_objects(objects_in) # Ingest the objects into the SDK
+            # Structure containing all the detected objects
+            print("sa ben geldim1")
             zed.retrieve_objects(objects, obj_runtime_params) # Retrieve the 3D tracked objects
+            print(objects.object_list)
+            print("sa ben geldim2")
             for object in objects.object_list:
-                print("{} {}".format(object.id, object.position))
-
+                print(f"{object.id} {object.position}")
+        else:
+            print("error with grabbing")
             
-            
-            
-
-
-
-
-    zed.close()
+if __name__ == "__main__":
+    main()
--- src/main2.py
+++ src/main2.py
@@ -1,3 +1,2 @@
 import torch
-torch.cuda.memory_summary(device=None, abbreviated=False)
-print(torch.PYTORCH_CUDA_ALLOC_CONF)
+print(torch.cuda.is_available())
